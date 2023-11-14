050d574d9aa002e9a81649251ee71afe3bfb6548
Elsanito <gacornito@gmail.com>
2023-11-14 12:19:30 +0700

    Develop Tasklist View

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -22,3 +11,16 @@ Route::get('/', function () {
     return view('home'); // Diperbarui
 
 });
+
+use App\Http\Controllers\TaskController; // Ditambahkan
+
+
+
+Route::get('/', function () {
+
+    return view('home');
+
+});
+
+
+Route::get('/tasks/', [TaskController::class, 'index']); // Ditambahkan
